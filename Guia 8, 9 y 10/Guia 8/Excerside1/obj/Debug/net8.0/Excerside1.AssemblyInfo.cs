55843ef7a9976b5571c76b8e6ce7074f6273e059
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 14:45:56 -0600

    Update project files and configurations for .NET 8.0 compatibility

--- Guia 8, 9 y 10/Guia 8/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
+++ Guia 8, 9 y 10/Guia 8/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186da98c68ba30e4cf2f251634d1fd86e38bc138")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a21353f06b291306c5dda3f3b0ef14d048c5c209")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
