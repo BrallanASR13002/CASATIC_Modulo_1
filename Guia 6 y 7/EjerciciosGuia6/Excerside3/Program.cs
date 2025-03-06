a1c5018ce5ca7493cd305b2c947b3faaf22d0f6c
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 14:03:54 -0600

    Implement main program structure with control flow and user interaction

--- Guia 6 y 7/EjerciciosGuia6/Excerside3/Program.cs
+++ Guia 6 y 7/EjerciciosGuia6/Excerside3/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.Clear();
+        Console.WriteLine("Estructuras de control repetitivas");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.WriteLine("Repetitiva condicional: for");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Conteo del 1 al 10");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("iteración: "+i);
+        }
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.ForegroundColor = color;
+    }
+}
