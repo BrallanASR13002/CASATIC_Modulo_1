c6d899bb09979bed1e2918003f6b970f13392710
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 14:28:01 -0600

    Add factorial calculation feature to console application

--- Guia 6 y 7/EjerciciosGuia6/Excerside4/Program.cs
+++ Guia 6 y 7/EjerciciosGuia6/Excerside4/Program.cs
@@ -5,7 +5,24 @@
         ConsoleColor color = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.DarkYellow;
         Console.Clear();
-
+        Console.WriteLine("Estructuras de control repetitivas");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();  
+        Console.Clear();
+        Console.WriteLine("Factorial de un número");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Ingrese un número entero: ");
+        int number1 = 0;
+        number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Numero ingresado: {number1}");
+        Console.WriteLine("-----------------------------------------------------------");
+        int factorial = 1;
+        for (int i = 1; i <= number1; i++)
+        {
+            factorial *= i;
+        }
+        Console.WriteLine($"El factorial de {number1} es: {factorial}");
         Console.WriteLine("-----------------------------------------------------------");
         Console.WriteLine("Presione una tecla para continuar...");
         Console.ReadKey();
