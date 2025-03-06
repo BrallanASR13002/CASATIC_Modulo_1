58ed81b42df61f8ada99180b6451c7dc8855dd05
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 12:23:04 -0600

    Implement basic calculator functionality with addition, subtraction, multiplication, and division

--- Guia 5/BasicCalculator/Program.cs
+++ Guia 5/BasicCalculator/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Clear();
+        Console.WriteLine("Basic Calculator");
+        Console.WriteLine("----------------");
+        Console.WriteLine("Enter the first number:");
+        double num1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number:");
+        double num2 = double.Parse(Console.ReadLine());
+        Console.WriteLine($"Sum of {num1} and {num2}: " + (num1 + num2));
+        Console.WriteLine($"Subtraction of {num1} and {num2}: " + (num1 - num2));
+        Console.WriteLine($"Multiplication between {num1} and {num2}: " + (num1 * num2));
+        double Division = num1 / num2;
+        Console.WriteLine($"Division of {num1} between {num2}: " + Math.Round(Division, 2));
+        Console.ForegroundColor = color;
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+        Console.Clear();
+
+    }
+}
