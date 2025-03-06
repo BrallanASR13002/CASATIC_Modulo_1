6c675c134b5cf2380f4f12274d7efe447d2c511b
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 12:34:03 -0600

    Implement value comparison functionality in console application

--- Guia 5/ValueCompare/Program.cs
+++ Guia 5/ValueCompare/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Clear();
+        Console.WriteLine("Comparación de valores");
+        Console.WriteLine("Ingrese el primer valor:");
+        string valor1 = Console.ReadLine();
+        Console.WriteLine("Ingrese el segundo valor:");
+        string valor2 = Console.ReadLine();
+        Console.WriteLine("Mayor que:");
+        valor1 > valor2 ? Console.WriteLine("El primer valor es mayor que el segundo") : Console.WriteLine("El segundo valor es mayor que el primero");
+        Console.WriteLine("Menor que:");
+        valor1 < valor2 ? Console.WriteLine("El primer valor es menor que el segundo") : Console.WriteLine("El segundo valor es menor que el primero");
+        Console.WriteLine("Igual que:");
+        valor1 == valor2 ? Console.WriteLine("Los valores son iguales") : Console.WriteLine("Los valores son diferentes");
+        Console.WriteLine("Distinto que:");
+        valor1 != valor2 ? Console.WriteLine("Los valores son diferentes") : Console.WriteLine("Los valores son iguales");
+        Console.ForegroundColor = color;
+        Console.WriteLine("Presione una tecla para salir");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
