087ee72f1b44a4e826ef6a588f0ec3d7e853fa27
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-06 13:58:16 -0600

    Implement control structures in console application with user input handling

--- Guia 6 y 7/EjerciciosGuia6/Excerside2/Program.cs
+++ Guia 6 y 7/EjerciciosGuia6/Excerside2/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.Clear();
+        Console.WriteLine("Estructuras de control condicionales");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.WriteLine("Selectiva múltiple: switch");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.WriteLine("Ingrese un número entero: ");
+        int number1 = 0;
+        number1 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Numero ingresado: {number1}");
+        Console.WriteLine("-----------------------------------------------------------");
+        switch (number1)
+        {
+            case 1:
+                Console.WriteLine("El día es lunes");
+                break;
+            case 2:
+                Console.WriteLine("El día es martes");
+                break;
+            case 3:
+                Console.WriteLine("El día es miércoles");
+                break;
+            case 4:
+                Console.WriteLine("El día es jueves");
+                break;
+            case 5:
+                Console.WriteLine("El día es viernes");
+                break;
+            case 6:
+                Console.WriteLine("El día es sábado");
+                break;
+            case 7:
+                Console.WriteLine("El día es domingo");
+                break;  
+            default:
+                Console.WriteLine("El número ingresado no corresponde a un día de la semana");
+                break;
+        }
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.ForegroundColor = color;
+    }
+}
