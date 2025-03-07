1eb44bf2a5151ab608c6449f873ce74ce49c5f33
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 08:30:12 -0600

    Implement initial Program class with DataInput and Print methods for user input and formatted output

--- Guia 8, 9 y 10/Guia 10/Excerside2/Program.cs
+++ Guia 8, 9 y 10/Guia 10/Excerside2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        public static double DataInpunt()
+        {
+            double number;
+            string data;
+            Print("Ingrese un numero: ");
+            data = Console.ReadLine();
+            number = Convert.ToDouble(data);
+            return number;
+        }
+        public static string Print(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine(message);
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.ForegroundColor = color;
+            return message;
+        }
+    }
+}
