f4dd3e26b796e7b0c56efcbb5b771130d9b820c6
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 08:28:31 -0600

    Add Excerside2 project with initial Program.cs and project files

--- Guia 8, 9 y 10/Guia 10/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
+++ Guia 8, 9 y 10/Guia 10/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c685d5f079d82ebb6cff8f836687a06caad359e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06424617944fa83485eaeade68b69aea4c6904cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
