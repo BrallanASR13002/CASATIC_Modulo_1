418c7a471fc58dba886f8c4e3c0b9b47f60dc7db
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:36:22 -0600

    Add functionality to input an array of integers and calculate their sum

--- Guia 8, 9 y 10/Guia 9/Excerside1/Program.cs
+++ Guia 8, 9 y 10/Guia 9/Excerside1/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Diagnostics.CodeAnalysis;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -6,6 +8,26 @@
         Console.ForegroundColor = ConsoleColor.DarkCyan;
         Console.Clear();
         Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("arreglo de numeros enteros y calcular la suma de los elementos");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Ingrese la cantidad de elementos del arreglo: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arreglo = new int[n];
+        int suma = 0;
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Ingrese el elemento {0} del arreglo: ", i + 1);
+            arreglo[i] = int.Parse(Console.ReadLine());
+            suma += arreglo[i];
+        }
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("El arreglo ingresado es: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("La suma de los elementos del arreglo es: {0}", suma); 
         Console.WriteLine("-----------------------------------------------------------");
         Console.WriteLine("Presione una tecla para continuar...");
         Console.ReadKey();
