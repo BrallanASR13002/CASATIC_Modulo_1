ca382493965b07c7f44f010d56bd544498c0205a
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:38:05 -0600

    Update project files for .NET 8.0 compatibility and regenerate caches

--- Guia 8, 9 y 10/Guia 9/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
+++ Guia 8, 9 y 10/Guia 9/Excerside1/obj/Debug/net8.0/Excerside1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d412616f09013b66b39e16a13da793964b998be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+418c7a471fc58dba886f8c4e3c0b9b47f60dc7db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Excerside1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
