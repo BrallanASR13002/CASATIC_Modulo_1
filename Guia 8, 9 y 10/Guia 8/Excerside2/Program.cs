8a7e456a512b6dd39900a2cbdf956c320b750498
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:11:58 -0600

    Add character count prompt to Excerside2 console application

--- Guia 8, 9 y 10/Guia 8/Excerside2/Program.cs
+++ Guia 8, 9 y 10/Guia 8/Excerside2/Program.cs
@@ -6,7 +6,8 @@
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.Clear();
         Console.WriteLine("-----------------------------------------------------------");
-
+        Console.WriteLine("Conteo de caracteres en una cadena de texto");
+        
         Console.WriteLine("-----------------------------------------------------------");
         Console.WriteLine("Presione una tecla para continuar...");
         Console.ReadKey();
