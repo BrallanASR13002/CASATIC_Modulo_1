0d9e64524e742d9eda0fc84e7dae846156740c9d
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:08:39 -0600

    Initialize Excerside2 project with .NET 8.0 and basic console output

---
+++ Guia 8, 9 y 10/Guia 8/Excerside2/Program.cs
@@ -0,0 +1,10 @@
+﻿ConsoleColor color = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Clear();
+Console.WriteLine("-----------------------------------------------------------");
+
+Console.WriteLine("-----------------------------------------------------------");
+Console.WriteLine("Presione una tecla para continuar...");
+Console.ReadKey();
+Console.Clear();
+Console.ForegroundColor = color;
