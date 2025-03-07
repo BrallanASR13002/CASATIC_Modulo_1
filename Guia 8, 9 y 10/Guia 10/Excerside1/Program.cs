31ccdbcdb4d6af617166367610dc4396e9a75e44
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:56:47 -0600

    Refactor Program class to improve console output and user interaction

--- Guia 8, 9 y 10/Guia 10/Excerside1/Program.cs
+++ Guia 8, 9 y 10/Guia 10/Excerside1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Clear();
+        Console.WriteLine("-----------------------------------------------------------");
+
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.ForegroundColor = color;
+    }
+}
