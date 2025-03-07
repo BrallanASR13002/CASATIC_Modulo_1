af3fca76f015d636375f9ac9232cfe1db5317518
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 12:03:50 -0600

    Refactor formatting and structure of AreaCircle, Print, and DataInput methods for improved readability

--- Guia 8, 9 y 10/Guia 10/Excerside2/Program.cs
+++ Guia 8, 9 y 10/Guia 10/Excerside2/Program.cs
@@ -9,27 +9,27 @@
         result = AreaCircle(radius);
         Print($"El area de un circulo con radio {radius} es: " + result);
     }
-     public static double DataInpunt()
-        {
-            double number;
-            string data;
-            Print("Ingrese el valor del radio: ");
-            data = Console.ReadLine();
-            number = Convert.ToDouble(data);
-            return number;
-        }
-        public static string Print(string message)
-        {
-            ConsoleColor color = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.WriteLine(message);
-            Console.WriteLine("-----------------------------------------------------------");
-            Console.ForegroundColor = color;
-            return message;
-        }
-        public static double AreaCircle(double radius)
-        {
-            return Math.Round(Math.PI * Math.Pow(radius, 2),2);
-        }
+    public static double DataInpunt()
+    {
+        double number;
+        string data;
+        Print("Ingrese el valor del radio: ");
+        data = Console.ReadLine();
+        number = Convert.ToDouble(data);
+        return number;
     }
+    public static string Print(string message)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine(message);
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.ForegroundColor = color;
+        return message;
+    }
+    public static double AreaCircle(double radius)
+    {
+        return Math.Round(Math.PI * Math.Pow(radius, 2), 2);
+    }
+}
