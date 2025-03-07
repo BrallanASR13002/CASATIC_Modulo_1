b00f3a52a715423664bcff4cd459df34aa611362
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:51:04 -0600

    Add functionality to find and display the maximum value in an array of integers

--- Guia 8, 9 y 10/Guia 9/Excerside2/Program.cs
+++ Guia 8, 9 y 10/Guia 9/Excerside2/Program.cs
@@ -6,7 +6,29 @@
         Console.ForegroundColor = ConsoleColor.DarkGreen;
         Console.Clear();
         Console.WriteLine("-----------------------------------------------------------");
-
+        Console.WriteLine("Valor maximo de un arreglo de numeros enteros");
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Ingrese la cantidad de elementos del arreglo: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arreglo = new int[n];
+        int max = int.MinValue;
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Ingrese el elemento {0} del arreglo: ", i + 1);
+            arreglo[i] = int.Parse(Console.ReadLine());
+            if (arreglo[i] > max)
+            {
+                max = arreglo[i];
+            }
+        }
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("El arreglo ingresado es: ");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arreglo[i] + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("El valor maximo del arreglo es: {0}", max);
         Console.WriteLine("-----------------------------------------------------------");
         Console.WriteLine("Presione una tecla para continuar...");
         Console.ReadKey();
