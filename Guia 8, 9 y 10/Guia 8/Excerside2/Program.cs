6df0d129f7ffd647e30fcf394a290175c35ee741
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-07 07:09:12 -0600

    Refactor Program class structure and encapsulate Main method

--- Guia 8, 9 y 10/Guia 8/Excerside2/Program.cs
+++ Guia 8, 9 y 10/Guia 8/Excerside2/Program.cs
@@ -1,10 +1,16 @@
-﻿ConsoleColor color = Console.ForegroundColor;
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.Clear();
-Console.WriteLine("-----------------------------------------------------------");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleColor color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Clear();
+        Console.WriteLine("-----------------------------------------------------------");
 
-Console.WriteLine("-----------------------------------------------------------");
-Console.WriteLine("Presione una tecla para continuar...");
-Console.ReadKey();
-Console.Clear();
-Console.ForegroundColor = color;
+        Console.WriteLine("-----------------------------------------------------------");
+        Console.WriteLine("Presione una tecla para continuar...");
+        Console.ReadKey();
+        Console.Clear();
+        Console.ForegroundColor = color;
+    }
+}
